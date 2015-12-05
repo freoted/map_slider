ee86a60a1cd933dade121212260a77339c709f8c
Rosie Bell <mrsrosiebell@gmail.com>
2015-12-05 21:20:24 +0800

    loads more changes

--- js/timeline_buildings.js
+++ js/timeline_buildings.js
@@ -1,19 +1,25 @@
 var map;
 
+// declare function for slider
+// this slider 
+function outputUpdate(vol) {
+	document.querySelector('#year').value = vol;
+	}
 
-
-	
+// function to initialise the map	
 function initmap() {
 	// set up the map
 	map = new L.Map('map');
 
 	// create the tile layer with correct attribution
 	var osm = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', 
-	{minZoom: 1, maxZoom: 19, attribution:'&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+	{minZoom: 1, maxZoom: 19, attribution:'&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
     subdomains: ['otile1','otile2','otile3','otile4']}).addTo( map );		
 
 	// start the map in Northbridge
 	map.setView(new L.LatLng(-31.949630, 115.859172),19);
+	
+	// add tiles
 	map.addLayer(osm);
 
 }
@@ -21,6 +27,7 @@ function initmap() {
 function popUpInfo (feature, layer) {
 	layer.bindPopup(feature.properties.name_and_business);
 };
+
 //function to load custom markers
 function customIcons(feature, latlng) {
 var myIcon = L.icon({
@@ -33,7 +40,7 @@ return L.marker(latlng, {icon: myIcon});
 initmap();
 
 //load geoJson layer - call functions to load current markers and get info to pop ups
-L.geoJson(geo_1908, {
+L.geoJson(geo_2015, {
 	pointToLayer: customIcons,
 	onEachFeature: popUpInfo
 }).addTo(map);
